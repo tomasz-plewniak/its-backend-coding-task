09000935aa6fddba35c91907b4337a1fbd70a920
Tomasz Plewniak <t_plewniak@yahoo.com>
2024-03-03 22:05:25 +0100

    Validate claim damage cost.

---
+++ src/WebAPI/Validators/ClaimValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using WebAPI.DTOs;
+
+namespace WebAPI.Validators;
+
+public class ClaimValidator : AbstractValidator<Claim>
+{
+    private const decimal MaxDamageCost = 100000;
+    
+    public ClaimValidator()
+    {
+        RuleFor(x => x.DamageCost).LessThan(MaxDamageCost)
+            .WithMessage($"DamageCost cannot exceed {MaxDamageCost}");
+    }
+}
---
+++ tests/UnitTests/WebApi/Validators/ClaimValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation.TestHelper;
+using WebAPI.DTOs;
+using WebAPI.Validators;
+
+namespace UnitTests.WebApi.Validators;
+
+public class ClaimValidatorTests
+{
+    private ClaimValidator _claimValidator;
+    
+    public ClaimValidatorTests()
+    {
+        _claimValidator = new ClaimValidator();    
+    }
+    
+    [Theory]
+    [InlineData(100000)]
+    [InlineData(100001)]
+    [InlineData(123001)]
+    [InlineData(1000000)]
+    [InlineData(10000000)]
+    public void ShouldHaveErrorWhenDamageCostExceed100000(decimal damageCost)
+    {
+        var model = new Claim() { DamageCost = damageCost };
+        
+        var result = _claimValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(claim => claim.DamageCost);
+    }
+    
+    [Theory]
+    [InlineData(99999)]
+    [InlineData(1)]
+    [InlineData(2332)]
+    [InlineData(999)]
+    [InlineData(23342)]
+    public void ShouldNotHaveErrorWhenDamageCostIsntExceed100000(decimal damageCost)
+    {
+        var model = new Claim() { DamageCost = damageCost };
+        
+        var result = _claimValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(claim => claim.DamageCost);
+    }
+}
