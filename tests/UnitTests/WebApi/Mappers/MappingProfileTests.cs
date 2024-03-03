69cbbfb154261bd08fd4ff22ae5c83ff5e21c01a
Tomasz Plewniak <t_plewniak@yahoo.com>
2024-03-03 21:37:08 +0100

    Add testing of mapping profile.

---
+++ tests/UnitTests/WebApi/Mappers/MappingProfileTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using WebAPI.Mappers;
+
+namespace UnitTests.WebApi.Mappers;
+
+public class MappingProfileTests
+{
+    [Fact]
+    public void ValidateClaimProfileMappingConfigurationTest()
+    {
+        MapperConfiguration mapperConfig = new MapperConfiguration(
+            cfg =>
+            {
+                cfg.AddProfile(new ClaimProfile());
+            });
+
+        IMapper mapper = new Mapper(mapperConfig);
+
+        mapper.ConfigurationProvider.AssertConfigurationIsValid();
+    }
+    
+    [Fact]
+    public void ValidateCoverProfileMappingConfigurationTest()
+    {
+        MapperConfiguration mapperConfig = new MapperConfiguration(
+            cfg =>
+            {
+                cfg.AddProfile(new CoverProfile());
+            });
+
+        IMapper mapper = new Mapper(mapperConfig);
+
+        mapper.ConfigurationProvider.AssertConfigurationIsValid();
+    }
+}
