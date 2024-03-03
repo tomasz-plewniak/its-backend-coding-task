efcb39ecbfe9782efbc3871456ee1fbb37480fce
Tomasz Plewniak <t_plewniak@yahoo.com>
2024-03-03 14:20:06 +0100

    Move entities into core project.

---
+++ src/ApplicationCore/Entities/ClaimAudit.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationCore.Entities;
+
+public class ClaimAudit
+{
+    public int Id { get; set; }
+
+    public string? ClaimId { get; set; }
+
+    public DateTime Created { get; set; }
+
+    public string? HttpRequestType { get; set; }
+}
---
+++ src/ApplicationCore/Entities/Cover.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Shared.Enums;
+
+namespace ApplicationCore.Entities;
+
+public class Cover
+{
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; }
+
+    [JsonProperty(PropertyName = "startDate")]
+    public DateOnly StartDate { get; set; }
+    
+    [JsonProperty(PropertyName = "endDate")]
+    public DateOnly EndDate { get; set; }
+    
+    [JsonProperty(PropertyName = "claimType")]
+    public CoverType Type { get; set; }
+
+    [JsonProperty(PropertyName = "premium")]
+    public decimal Premium { get; set; }
+}
