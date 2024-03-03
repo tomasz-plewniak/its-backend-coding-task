de29743039558c888ef26b4feeadfe765e36b061
Tomasz Plewniak <t_plewniak@yahoo.com>
2024-03-03 14:35:55 +0100

    Migrate infrastructure.

---
+++ src/Infrastructure/SQLDatabase/AuditContext.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.SQLDatabase
+{
+    public class AuditContext : DbContext
+    {
+        public AuditContext(DbContextOptions<AuditContext> options) : base(options)
+        {
+        }
+        public DbSet<ClaimAudit> ClaimAudits { get; set; }
+        public DbSet<CoverAudit> CoverAudits { get; set; }
+    }
+}
